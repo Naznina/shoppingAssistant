09c3c58dcb3c68d4aa6069a08786bf9f345a28d1
hnaznina <hoque.naznina@gmail.com>
2018-04-24 23:03:45 -0500

    Link up Item and Mart Page from tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 
+//import { ContactPage } from '../contact/contact';
+//import { HomePage } from '../home/home';
+import { ItemPage } from '../item/item';
+import { MartPage } from '../mart/mart';
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = ItemPage;
+  tab2Root = MartPage;
+  tab3Root = AboutPage;
 
   constructor() {
 
